426b5a93cfee400b1475a44a029e47b0ea048b2d
Don Cooper <don_cooper@hotmail.com>
2017-07-12 15:48:36 -0400

    AJAX request working

--- cats.js
+++ cats.js
@@ -2,6 +2,15 @@ document.addEventListener("DOMContentLoaded", function() {
 
   var catButton = document.querySelector('.summon-cats');
 
+  catButton.addEventListener('click', function() {
+    $.ajax({
+      url: 'http://bitkittens.herokuapp.com/cats.json',
+      method: 'GET',
+      data: {},
+      dataType: 'json'
+    });
+  });
+
 
 
 
